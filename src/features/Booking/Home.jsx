ee60cd2042443cdaf835a83ec01087492598bfc2
minhtoan <tranminhtoan963@gmail.com>
2023-01-10 08:03:11 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/app/api.js 	new file:   src/app/apiPath.js 	new file:   src/app/store.js 	new file:   src/components/Header.jsx 	new file:   src/features/Booking/Home.jsx 	new file:   tailwind.config.js

---
+++ src/features/Booking/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home
+      <Button type='primary' className='text-red-600 text-9xl'>abcdr</Button>
+    </div>
+  )
+}
+
+export default Home
