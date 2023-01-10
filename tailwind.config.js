678a354fbd861c14ffaaf14ba7673246036827b5
minhtoan <tranminhtoan963@gmail.com>
2023-01-10 08:04:52 +0700

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  // tắt preflight để dùng chung với Ant Design
+
+  corePlugins: {
+    preflight: false,
+  },
+};
