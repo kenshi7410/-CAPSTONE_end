d200ec5d270ed7b2ace382dfa086d7c15ccf2b28
minhtoan <tranminhtoan963@gmail.com>
2023-01-11 07:35:17 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/app/apiPath.js 	modified:   src/app/store.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./features/Booking/Home";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Header />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/app/apiPath.js
@@ -0,0 +1,3 @@
+const apiPath = {};
+
+export default apiPath;
